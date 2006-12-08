40e47400a8c97758e7b49186fc5fb3a62e037316
ronw <ronw@ee.columbia.edu>
2006-12-08 01:36:33 -0500

    train_gmm_htk now calls train_hmm_htk
    darcs-hash:20061208063633-4ed31-d840a26c9f4d01b6d4e6107db47b306dd6d6b338.gz

--- train_gmm_htk.m
+++ train_gmm_htk.m
@@ -17,8 +17,6 @@ function gmm = train_gmm_htk(trdata, nmix, niter, verb, CVPRIOR, mu0);
 %
 % 2006-12-06 ronw@ee.columbia.edu
 
-
-% based on trainhmmhtk.m
 if nargin < 2
   nmix = 3;
 end
@@ -75,57 +73,6 @@ end
 gmm.covars = ones(ndim, nmix);
 hmm.gmms = {gmm};
 
-% write temp files for each sequence
-
-% Temporary file to use
-rnd = num2str(round(10000*rand(1)));
-for n = 1:length(trdata)
-  datafilename{n} = ['/tmp/matlabtmp_htkdat_' rnd '_' num2str(n) ...
-        '.dat']; 
-
-  % custom data format:
-  htkcode = 9;  % USER
-  %htkcode = 6;  % MFCC
-  htkwrite(trdata{n}', datafilename{n}, htkcode);
-end
-
-% initial HTK HMM: HInit/HCompV? 
-hmmfilename = ['/tmp/matlabtmp_htkhmm_' rnd];
-write_htk_hmm(hmmfilename, hmm); 
-use_hinit = 0;
-if use_hinit
-  args = ['-i ' num2str(niter) ' -v ' num2str(CVPRIOR)];
-  args = [args ' -T 1'];
-  if verb > 1
-    args = [args ' -A -V -D'];
-  end
-  disp('running HInit...');
-  datestr(now)
-  system(['HInit ' args ' ' hmmfilename ' ' sprintf('%s ', datafilename{:})]);
-  datestr(now)
-  disp('done.');
-end 
-
-% run HRest to train:
-args = ['-t -M /tmp -i ' num2str(niter) ' -v ' num2str(CVPRIOR)];
-if verb
-  args = [args ' -T 3'];
-  if verb > 1
-    args = [args ' -A -V -D'];
-  end
-end
-system([get_htk_path() 'HRest ' args ' ' hmmfilename ' ' sprintf('%s ', datafilename{:})]);
-
-if verb
-  disp(['******** DONE ********'])
-end
-
-% read in hmm:
-hmm = read_htk_hmm(hmmfilename);
+hmm = train_hmm_htk(trdata, hmm, niter, verb, CVPRIOR);
 gmm = hmm.gmms{1};
 
-% clean up:
-system(['rm ' hmmfilename]);
-system(['rm ' sprintf('%s ', datafilename{:})]);
-
-% done.
